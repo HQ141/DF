8e5e00e4b451f5a906a4afc3c6e659065578bdef
KaizukoOni <kaizukooni@HQ.Home>
2022-05-28 16:23:12 +0500

    Integrity

--- Image_Compress.py
+++ Image_Compress.py
@@ -3,9 +3,11 @@ from os import _EnvironCodeFunc
 import tarfile
 import time
 import argparse
+import requests
 par=argparse.ArgumentParser()
 par.add_argument('file')
 par.add_argument('-b')
+par.add_argument('-url')
 out=par.parse_args()
 src=open(out.file,'rb')
 tm =str(time.time())
@@ -18,6 +20,10 @@ while by:
     hash_al.update(by)
 hash_al.update(tm.encode())
 print(hash_al.hexdigest())
+url=out.url
+value={'hex':hash_al.hexdigest()}
+if url:
+    requests.post(url,data=value)
 src.close()
 evidence.close()
 compres=tarfile.open('Image.tar','w:xz')
