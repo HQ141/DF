3206c8a53c7eb6cffd9da2a05740307990a97bf6
root <root@HQ.Home>
2022-06-10 20:13:38 +0500

    Integrity

--- Image_Compress.py
+++ Image_Compress.py
@@ -14,16 +14,18 @@ tm =str(time.time())
 by=b'x00'
 evidence=open("Image",'wb')
 hash_al=hashlib.md5()
+salt='qwerty'
 while by:
     by=src.read(out.b)
     evidence.write(by)
     hash_al.update(by)
 hash_al.update(tm.encode())
+hash_al.update(salt.encode())
 print(hash_al.hexdigest())
 url=out.url
 value={'hex':hash_al.hexdigest()}
 if url:
-    requests.post(url,data=value)
+    catrequests.post(url,data=value)
 src.close()
 evidence.close()
 compres=tarfile.open('Image.tar','w:xz')
