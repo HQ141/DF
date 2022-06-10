bce4fa1f1f6515ae0f958f3b9ac2e246b61740e6
KaizukoOni <kaizukooni@HQ.Home>
2022-06-11 01:31:55 +0500

    Reup

--- Memory_Image.py
+++ Memory_Image.py
@@ -1,24 +1,60 @@
+from genericpath import isdir
+import os
 import sys
 import argparse
 par=argparse.ArgumentParser()
-par.add_argument('pid')
+par.add_argument('name')
+par.add_argument('-p')
+par.add_argument('-r')
 out=par.parse_args()
-pid=out.pid
-map_file=f"/proc/{pid}/maps"
-mem_file=f"/proc/{pid}/mem"
-out_mem=f"{pid}.dump"
-out_map=f"{pid}.map"
-mapf=open(map_file,'r')
-memf=open(mem_file,'rb')
-outf=open('ww','wb')
-for line in mapf:
-    section=line.split(' ')
-    if(section[1][0]=='r'):
-        address=section[0].split('-')
-        st=int(address[0],16)
-        end=int(address[1],16)
-        memf.seek(st)
-        try:
-            outf.write(memf.read(end-st))
-        except OSError:
-            print("skipped"+hex(st)+'-'+hex(end)+'\n')
+pid=out.p
+name=out.name
+rag=out.r
+if(os.path.isdir("Mem_dumps")==0):
+    os.mkdir("Mem_dumps")
+if(pid):
+    i=pid
+    if(os.path.isfile(f"/proc/{i}/maps")):
+        map_file=f"/proc/{i}/maps"
+        mem_file=f"/proc/{i}/mem"
+        out_mem=f"{i}.dump"
+        out_map=f"{i}.map"
+        mapf=open(map_file,'r')
+        memf=open(mem_file,'rb')
+        outf=open(f'Mem_dumps/{name}.{i}','wb')
+        for line in mapf:
+            section=line.split(' ')
+            if(section[1][0]=='r'):
+                address=section[0].split('-')
+                st=int(address[0],16)
+                end=int(address[1],16)
+                memf.seek(st)
+                try:
+                    outf.write(memf.read(end-st))
+                except OSError:
+                    print("skipped"+hex(st)+'-'+hex(end)+'\n')
+else:
+    rag=rag.split('-')
+    s=int(rag[0])
+    e=int(rag[1])
+    for i in range(s,e+1):
+        print(i)
+        if(os.path.isfile(f"/proc/{i}/maps")):
+            map_file=f"/proc/{i}/maps"
+            mem_file=f"/proc/{i}/mem"
+            out_mem=f"{i}.dump"
+            out_map=f"{i}.map"
+            mapf=open(map_file,'r')
+            memf=open(mem_file,'rb')
+            outf=open(f'Mem_dumps/{name}.{i}','wb')
+            for line in mapf:
+                section=line.split(' ')
+                if(section[1][0]=='r'):
+                    address=section[0].split('-')
+                    st=int(address[0],16)
+                    end=int(address[1],16)
+                    memf.seek(st)
+                    try:
+                        outf.write(memf.read(end-st))
+                    except OSError:
+                        print("skipped"+hex(st)+'-'+hex(end)+'\n')
---
+++ ss.py
@@ -0,0 +1,24 @@
+import os
+import socket
+import pwd
+import json
+from anyio import connect_tcp
+
+from psycopg2 import connect
+import fabric
+from cairo import HAS_QUARTZ_SURFACE
+host="192.168.10.16"
+with open("instances.json") as jfile:
+    jdata=json.load(jfile)
+    for i in jdata:
+        Username=jdata[i]['ssh']['username']
+        password=jdata[i]['ssh']['password']
+        port=jdata[i]['ssh']['port']
+        print(Username)
+        print(password)
+        print(port)
+Username='hq'
+password='qwerty'
+port='22'
+c=fabric.Connection(f"{Username}@{host}:22",connect_kwargs={'password':f'{password}'})
+c.run("sshpass -p 'RainGOaway' scp -P 22 kaizukooni@192.168.10.12:~/Desktop/a ~/Desktop")
